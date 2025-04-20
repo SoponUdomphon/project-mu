abf57665a6fc5d6a05f71da243cd0d8345a4272c
mr.sopon <mr.sopon.udo@gmail.com>
2025-04-20 12:07:32 +0700

    Add React project files

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to MU Website</h1>
+        <p>
+          This site is under construction.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
